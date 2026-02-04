f5a0e7b62a93b081cfcda201db5fed0f23521c2d
rlv-iith <ic23btech11016@iith.ac.in>
2026-02-04 23:59:45 +0530

    Time: 0 ms (100%), Space: 19.2 MB (49.21%) - LeetHub

---
+++ 1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.py
@@ -0,0 +1,8 @@
+class Solution:
+    def uniqueOccurrences(self, arr: List[int]) -> bool:
+        freq = {}
+
+        for num in arr:
+            freq[num] = freq.get(num, 0) + 1
+
+        return len(freq.values()) == len(set(freq.values()))
